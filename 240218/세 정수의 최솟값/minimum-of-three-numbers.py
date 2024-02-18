7ca8c0528321793eef740ffb3c3d4f036783e1c7
GUNAGI12 <153091828+GUNAGI12@users.noreply.github.com>
2024-02-18 20:37:15 +0900

    Title: 세 정수의 최솟값, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240218/세 정수의 최솟값/minimum-of-three-numbers.py
@@ -0,0 +1,14 @@
+inp = input()
+arr = inp.split()
+a = int(arr[0])
+b = int(arr[1])
+c = int(arr[2])
+
+if a<=b and a<=c :
+    print(a)
+
+elif b<=c and b<=a :
+    print(b)
+
+else :
+    print(c)
