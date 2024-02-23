c2d844d83d5ec0f1dddb2a0c2beae8f3a025e893
GUNAGI12 <153091828+GUNAGI12@users.noreply.github.com>
2024-02-23 17:37:21 +0900

    Title: 출석 부르기 2, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240223/출석 부르기 2/calling-attendance-2.py
@@ -0,0 +1,14 @@
+while True :
+    n = int(input())
+    if n>4 :
+        print("Vacancy")
+        break
+    
+    if n == 1 :
+        print("John")
+    if n == 2 :
+        print("Tom")
+    if n == 3 :
+        print("Paul")
+    if n == 4 :
+        print("Sam")
