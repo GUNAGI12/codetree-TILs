8ee12833c2511342d3ec397033181fcf8190a8f2
GUNAGI12 <153091828+GUNAGI12@users.noreply.github.com>
2024-03-04 13:55:25 +0900

    Title: 배열의 특정 위치 찾기 2, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240304/배열의 특정 위치 찾기 2/find-specific-location-fo-array-2.py
@@ -0,0 +1,13 @@
+arr = list(map(int, input().split()))
+sum1 = 0
+sum2 = 0
+for i in range(0, 9, 2) :
+    sum1 += arr[i]
+
+for ii in range(1,9,2) :
+    sum2 += arr[ii]
+
+if sum1 >= sum2 :
+    print(sum1 - sum2)
+else :
+    print(sum2-sum1)
